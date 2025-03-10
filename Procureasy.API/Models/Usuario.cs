5757fec01a37a87f9be552da020b741e345dc954
Pedro Silva <120214894+PedroXIX@users.noreply.github.com>
2025-03-10 00:46:30 -0300

    Create Usuario.cs

---
+++ Procureasy.API/Models/Usuario.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Procureasy.API.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Cnpj { get; set; }
+        public string Admin { get; set; }
+        public string Nome { get; set; }
+    }
+}
